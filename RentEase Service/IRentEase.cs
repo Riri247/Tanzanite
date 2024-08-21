99c0227d990401e90abf328c5e902a4e0f1fc50f
CodeBullAlpha <hemowesley@gmail.com>
2024-08-21 20:13:23 +0200

    Reece can you do this function with the help of helper function

--- RentEase Service/IRentEase.cs
+++ RentEase Service/IRentEase.cs
@@ -14,7 +14,10 @@ namespace RentEase_Service
 
         [OperationContract]
         void Login();
-
+        //function for getting stats for merchants 
+        //how many products they rented out 
+        //number of people who rented their products 
+        //and the profit they have made overall
         [OperationContract]
         void GetStats();
     }
