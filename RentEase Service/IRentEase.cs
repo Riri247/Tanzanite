7e9977929c5c6de6247a52a4174c259ca9a0ef0b
GeorgeMugale <georgeapril202020@gmail.com>
2024-08-25 13:36:01 +0200

    This update includes methods to complete the service.

---
+++ RentEase Service/IRentEase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RentEase_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRentEase" in both code and config file together.
+    [ServiceContract]
+    public interface IRentEase
+    {
+        
+        [OperationContract]
+        User Login(string email, string password);
+
+        [OperationContract]
+        bool Register(string email, string password, string name, string surname);
+
+        [OperationContract]
+        Product getProduct(int ID);
+
+        [OperationContract]
+        List<Product> getProducts();
+
+        [OperationContract]
+        bool addToCart(int UserID, int ProductID);
+
+        [OperationContract]
+        bool removeFromCart(int UserID, int ProductID);
+
+        [OperationContract]
+        List<Product> getUserCart(int ID);
+
+        [OperationContract]
+        User getUser(int ID);
+
+    }
+}
