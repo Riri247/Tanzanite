78506419d8753bd9a12ff6ae06bf076dc27d7eb8
Riri247 <risunamabasa058@gmail.com>
2024-08-27 21:03:19 +0200

    Wesley I'm done

--- FRONTEND/Home.aspx.cs
+++ FRONTEND/Home.aspx.cs
@@ -13,5 +13,33 @@ namespace FRONTEND
         {
 
         }
+
+        private void AddInitialProducts()
+        {
+            ServiceReference1.RentEaseClient rentEaseService = new ServiceReference1.RentEaseClient();
+            // Call your AddProduct method to insert products into the database
+            rentEaseService.AddProduct("Clarens Sofa", 10, 400.00m, 1);
+            rentEaseService.AddProduct("Baltimore Sofa", 10, 550.00m, 2);
+            rentEaseService.AddProduct("Montagu Sofa", 5, 450.00m, 3);
+            rentEaseService.AddProduct("Ashton Chair", 8, 370.00m, 4);
+            rentEaseService.AddProduct("Campbell Chair", 6, 400.00m, 5);
+            rentEaseService.AddProduct("Ballito Chair", 4, 200.00m, 6);
+            rentEaseService.AddProduct("Hamilton Chair", 4, 550.00m, 7);
+            rentEaseService.AddProduct("Centurion Table", 3, 550.00m, 8);
+            rentEaseService.AddProduct("Derby Table", 5, 200.00m, 9);
+            rentEaseService.AddProduct("Molteno Table", 12, 300.00m, 10);
+            rentEaseService.AddProduct("Designer Book Drawer", 10, 600.00m, 11);
+            rentEaseService.AddProduct("Lamp Stand", 4, 400.00m, 12);
+            rentEaseService.AddProduct("Designer Chairs", 8, 580.00m, 13);
+            rentEaseService.AddProduct("Orange Leather Couch", 3, 600.00m, 14);
+            rentEaseService.AddProduct("Computer Stand", 20, 300.00m, 15);
+            rentEaseService.AddProduct("George Lamp", 10, 300.00m, 16);
+            rentEaseService.AddProduct("Heidelberg Lamp", 10, 200.00m, 17);
+            rentEaseService.AddProduct("Infantana Lamp", 20, 150.00m, 18);
+            rentEaseService.AddProduct("Biggie Furniture Movers", 1, 1400.00m, 19);
+            rentEaseService.AddProduct("Storage Rentals", 40, 460.00m, 20);
+
+            // Add more products as needed
+        }
     }
 }
