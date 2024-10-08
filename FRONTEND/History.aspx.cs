9fd4512de3eb78e59af3e7f6d54d2ed658a92760
Emmanuel222127212 <112879564+Emmanuel222127212@users.noreply.github.com>
2024-10-08 13:07:46 +0200

    Get user Invoices
    Get user invoices wrapper class, and function , and dynamic allocation on each page

--- FRONTEND/History.aspx.cs
+++ FRONTEND/History.aspx.cs
@@ -4,14 +4,91 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FRONTEND.ServiceReference1;
 
 namespace FRONTEND
 {
     public partial class History : System.Web.UI.Page
     {
+
+        RentEaseClient Rs = new RentEaseClient();
         protected void Page_Load(object sender, EventArgs e)
         {
+            //if not reloaded load invoices for that user
+            if (!IsPostBack) {
+                LoadInvoices();
+            }
+        }
 
+        private void LoadInvoices()
+        {
+
+            //if they are logged inn generate the invoices else take them back to the home page
+            if (Session["ID"] != null) {
+                //maing the id a int
+                int CusID = Convert.ToInt32(Session["ID"].ToString());
+                dynamic ListInvoices = Rs.getUserInvoices(CusID);
+                String Scrollhtml = "";
+                if (ListInvoices != null) {
+                    foreach (GetInvoice gi in ListInvoices) {
+                        Scrollhtml = $@"<div class='Scrollable'>";
+
+ Scrollhtml = $@"<div class = 'NameBox'>";
+
+                        Scrollhtml = $@"<p> {gi.invID}</p>";
+
+                        Scrollhtml = $@"<p>{gi.invDate} </p>";
+
+
+                        Scrollhtml = $@"</div>";
+
+
+
+                        Scrollhtml = $@" <div class='btnbox'>";
+                        Scrollhtml = $@"< div class='anime__details__btn'>";
+
+                        Scrollhtml = $@"<a href='#' class='follow-btn'> View </a>";
+
+
+                        Scrollhtml = $@"</div>";
+
+                        Scrollhtml = $@"</div>";
+
+                        Scrollhtml = $@"</div>";
+
+                    }
+
+}                   divScrool.InnerHtml = Scrollhtml;
+
+            } else {
+                Response.Redirect("Home.aspx");
+            }
         }
     }
 }
+
+
+ //< div class= "Scrollable" >
+ 
+ //        < div  class = "NameBox" >
+  
+ //             < p > INvoice NUm </ p >
+    
+ //           < p > INvoice Date </ p >
+      
+
+ //                 </ div >
+      
+
+
+ //              < div class= "btnbox" >
+ //      < div class= "anime__details__btn" >
+        
+ //                                       < a href = "#" class= "follow-btn" > View </ a >
+           
+
+ //                                      </ div >
+           
+ //                      </ div >
+           
+ //              </ div >
